052e542b15229e6659b80a13104d11af72e1ff28
rose <novacosta@fastmail.com>
2026-02-22 06:44:47 +0000

    feat: add cross-link to Location from Asset edit screen
    - Add 'Open' link next to Location field when location is set
    - Opens Location app filtered by location name
    
    Related: strognoff/strognoff#13

--- src/components/EditAsset.jsx
+++ src/components/EditAsset.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import CopyIdField from './CopyIdField';
 
 const API_BASE = import.meta.env.VITE_ASSET_API || 'http://localhost:8080/restoam/assets';
+const LOCATION_APP_URL = import.meta.env.VITE_LOCATION_APP_URL || 'http://localhost:5174';
 
 function EditAsset() {
   const { id } = useParams();
@@ -78,7 +79,25 @@ function EditAsset() {
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="location" className="form-label">Location</label>
+          <label htmlFor="location" className="form-label">
+            Location
+            {formData.location && (
+              <a
+                href={`${LOCATION_APP_URL}?name=${encodeURIComponent(formData.location)}`}
+                target="_blank"
+                rel="noreferrer"
+                className="ms-2 text-primary"
+                title="Open Location"
+                style={{ fontSize: '0.875rem' }}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+                  <path d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+                </svg>
+                Open
+              </a>
+            )}
+          </label>
           <input
             type="text"
             className="form-control"
